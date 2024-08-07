b2c40b1bdd7e9c929c954a2a3866662c2d0f159e
byml-c <2014078563@qq.com>
2024-08-07 20:08:03 +0800

    update website

--- send.bat
+++ send.bat
@@ -1,11 +1,17 @@
-echo off
-echo "Uploading to server"
+@echo off
+echo Uploading to server
 python ./send.py
 
-echo "Uploading Github"
+echo Do you want to upload to Github? (y/n)
+set /p choice=
+if %choice%==y goto upload
+else goto finish
+
+:upload
+echo Uploading Github
 git add .
 git commit -m "update website"
 git push
 
-echo "Finish"
-pause
+:finish
+echo Finish
